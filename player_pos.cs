d7d9eda2aa58f98f4db92dda0da30473eff4b4d1
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 09:34:19 +0530

    Initializing player position to zero

---
+++ player_pos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKEandLADDER
+{
+   public class player_pos
+    {
+    public int player_position = 0;
+    }
+}
