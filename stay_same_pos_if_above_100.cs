96663fc7427a1c6ba978e79d82a6fdf25fe31120
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 09:45:44 +0530

    Stay in the same position if he goes above 100

---
+++ stay_same_pos_if_above_100.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKEandLADDER
+{
+    public class stay_same_pos_if_above_100
+    {
+        public void repeat(int playerposition,int dice)
+        {
+            while (playerposition < 100)
+            {
+                if (playerposition + dice <= 100)
+                    playerposition += dice;
+
+            }
+        }
+    }
+}
