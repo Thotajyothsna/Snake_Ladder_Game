37131787047ee2e8581f3b86fff2a6314f8108a8
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 09:37:50 +0530

    Generating Random Dice number

---
+++ Random_dice_num.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKEandLADDER
+{
+    public class Random_dice_num
+    {  
+        public int dice;
+        public int roll()
+        {
+            var rand=new Random();
+            dice=rand.Next(1,7);
+            return dice;
+        }
+    }
+}
