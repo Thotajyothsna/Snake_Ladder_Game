8a0cd4d35443d7df5b23c554b986e7e11ce440ae
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-05-06 09:51:48 +0530

    Position after each dice role and count

---
+++ dice_count.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKEandLADDER
+{
+    public class dice_count
+    {
+        public static int dice_cnt;
+        public int[] playerpositions=new int[dice_cnt];
+        public void display()
+        {
+            Console.WriteLine("no of time the player played the dice{0}",dice_cnt);
+            Console.WriteLine("player position after each dice roll is");
+            for (int i = 0; i < dice_cnt; i++)
+            {
+                Console.WriteLine(playerpositions[i]);
+            }
+        }
+    }
+}
